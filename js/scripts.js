8b738791da5136c037a78420d1caf5dce2d00c6c
StefanDellamore <dellamoreste.info@gmail.com>
2023-12-20 19:27:23 +0100

    Finalizzazione esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -55,3 +55,94 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById('container');
+
+
+for(let i = 0; i < posts.length; i++){
+
+    let imageTemp;
+
+    if(posts[i].author.image == null){
+
+        imageTemp = document.createElement('div');
+        imageTemp.classList.add('profile-pic-default');
+        imageTemp.innerHTML = `<span>${posts[i].author.name[0]}</span>`;
+        
+    }else{
+        imageTemp = document.createElement('img');
+        imageTemp.src = (posts[i].author.image);
+        imageTemp.alt = (posts[i].author.name);
+        imageTemp.classList.add('profile-pic');
+    }
+
+    container.innerHTML += `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author['name']}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="${posts[i].media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#nogo" data-postid="${posts[i].id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `
+        let iconUser = document.querySelector('.post:last-child .post-meta__icon');
+        iconUser.append(imageTemp) 
+        
+};
+
+let button = document.querySelectorAll('.like-button');
+
+const postIdList = [];
+
+
+for(let j = 0; j < button.length; j ++){
+
+    button[j].addEventListener('click', function(){
+
+        this.classList.toggle('like-button--liked');
+
+        if (this.classList.contains('like-button--liked')){
+
+            postIdList.push(posts[j].id);
+
+            let userLikeAdd = posts[j].likes + 1;
+            document.getElementById(`like-counter-${posts[j].id}`).innerHTML = userLikeAdd;
+        }
+
+        else {
+            
+            postIdList.shift(posts[j].id);
+            
+            let userLikeRemove = posts[j].likes;
+            document.getElementById(`like-counter-${posts[j].id}`).innerHTML = userLikeRemove;
+             
+            
+        }
+        
+        console.log(postIdList)
+
+    });
+
+}
